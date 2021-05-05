de233af62bbc779c0655b163eae3b13c35ab9373
Lassi Kortela <lassi@lassi.io>
2021-05-05 15:32:59 +0300

    Add another hello world

--- elforth.el
+++ elforth.el
@@ -19,8 +19,14 @@
 ;; know what is right and true are now liberated to program the One
 ;; True Editor in the One True Language.
 
+;; Try writing some El Forth in the *scratch* buffer, highlighting it,
+;; and doing M-x elforth-eval-region. For example:
+
+;; "hello" " " "world" 3 nlist 'concat apply
+
+;; Or if you're feeling festive:
+
 ;; "Party like it's " 1970 number-to-string concat "!" concat
-;; M-x elforth-eval-region
 
 ;;; Code:
 
